15e0dd207b80ee60fead9fd563ec94add1b45a17
vinayak <vinayak20130@gmail.com>
2024-11-03 16:49:54 +0530

    creating user and organization models

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { Request, Response } from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
-app.get('/', (req : Request ,res : Response) => {
-    res.send ("Working");
-})
-app.listen(port,() => {console.log(`app is running on port ${port}`)})
+import { PORT } from './secrets';
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working');
+});
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
